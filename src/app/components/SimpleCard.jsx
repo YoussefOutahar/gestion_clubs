ebdb38461c2d3ebaa61a4caf27c934d7bdc7b09c
ManalChailou <chailou.manal@gmail.com>
2023-06-06 14:44:08 +0100

    Finance Test

--- src/app/components/SimpleCard.jsx
+++ src/app/components/SimpleCard.jsx
@@ -1,4 +1,4 @@
-import { Card } from '@mui/material';
+import { Card, Icon } from '@mui/material';
 import { Box, styled } from '@mui/system';
 
 const CardRoot = styled(Card)(() => ({
@@ -7,18 +7,31 @@ const CardRoot = styled(Card)(() => ({
 }));
 
 const CardTitle = styled('div')(({ subtitle }) => ({
-  fontSize: '1rem',
-  fontWeight: '500',
+  fontSize: '25px',
+  fontWeight: '700',
   textTransform: 'capitalize',
   marginBottom: !subtitle && '16px',
+  color: '#1976d2'
+}));
+const ContentBox = styled(Box)(({ theme }) => ({
+  display: 'flex', 
+  flexWrap: 'wrap',
+  alignItems: 'center',
+  '& small': { color: theme.palette.text.secondary },
+  '& .icon': { opacity: 0.6, fontSize: '44px', color: theme.palette.primary.main },
 }));
 
 const SimpleCard = ({ children, title, subtitle, icon }) => {
   return (
     <CardRoot elevation={6}>
+      <ContentBox>
+      <Icon className="icon">{icon}</Icon>
+      <Box ml="12px">
       <CardTitle subtitle={subtitle}>{title}</CardTitle>
       {subtitle && <Box sx={{ mb: 2 }}>{subtitle}</Box>}
       {children}
+      </Box>
+      </ContentBox>
     </CardRoot>
   );
 };
