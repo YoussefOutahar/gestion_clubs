665d2b37f34c2ab1af2dae82f46ed9bc6a7f21ec
ed-daoudiManal22 <manaloglu@gmail.com>
2023-06-08 14:01:00 +0000

    clubs members

--- src/app/views/General/Clubs.js
+++ src/app/views/General/Clubs.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 const Clubs = () => {
+  const [showDetails, setShowDetails] = useState(false);
+  const [activeClub, setActiveClub] = useState(null);
+  const [joinFormVisible, setJoinFormVisible] = useState(false);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -30,72 +36,135 @@ const Clubs = () => {
     {
       logo: 'https://www.evaair.com/Images/vivn/the-club-logo_tcm43-68807.jpg',
       name: 'Club 1',
-      description: 'Description du Club 1',
-      buttonLabel: 'En savoir plus',
+      description: 'Description du Club 1 ',
+      buttonLabel: 'Join Us',
       background: 'white',
     },
     {
       logo: 'https://www.evaair.com/Images/vivn/the-club-logo_tcm43-68807.jpg',
       name: 'Club 2',
       description: 'Description du Club 2',
-      buttonLabel: 'En savoir plus',
+      buttonLabel: 'Join Us',
       background: 'white',
     },
     {
       logo: 'https://www.evaair.com/Images/vivn/the-club-logo_tcm43-68807.jpg',
       name: 'Club 3',
       description: 'Description du Club 3',
-      buttonLabel: 'En savoir plus',
+      buttonLabel: 'Join Us',
       background: 'white',
     },
     {
       logo: 'https://www.evaair.com/Images/vivn/the-club-logo_tcm43-68807.jpg',
       name: 'Club 4',
       description: 'Description du Club 4',
-      buttonLabel: 'En savoir plus',
+      buttonLabel: 'Join Us',
       background: 'white',
     },
     {
       logo: 'https://www.evaair.com/Images/vivn/the-club-logo_tcm43-68807.jpg',
       name: 'Club 5',
       description: 'Description du Club 5',
-      buttonLabel: 'En savoir plus',
+      buttonLabel: 'Join Us',
       background: 'white',
     },
     {
       logo: 'https://www.evaair.com/Images/vivn/the-club-logo_tcm43-68807.jpg',
       name: 'Club 6',
       description: 'Description du Club 6',
-      buttonLabel: 'En savoir plus',
+      buttonLabel: 'Join Us',
       background: 'white',
     },
   ];
 
+  const handleLearnMore = (index) => {
+    setActiveClub(index);
+    setShowDetails(true);
+  };
+
+  const handleBackToTop = () => {
+    setShowDetails(false);
+    setActiveClub(null);
+    setJoinFormVisible(false);
+  };
+
+  const handleJoinUs = (index) => {
+    setActiveClub(index);
+    setJoinFormVisible(true);
+  };
+
   return (
     <div className="clubs">
-      <h1 style={{ fontFamily: 'Montserrat, sans-serif', letterSpacing: '2px', fontSize: '35px', fontWeight: '400', margin: '15px 10px' }}>Clubs</h1>
-      <Slider {...settings}>
-        {clubs.map((club, index) => (
-          <div className="card-wrapper" key={index}>
-            <div className="card" style={{ background: club.background }}>
-              <div className="circle">
-                <img src={club.logo} alt={`Logo ${club.name}`} />
+      <h1>Clubs</h1>
+      {showDetails ? (
+        <div>
+          <div onClick={handleBackToTop} className="back-icon">
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </div>
+          <div className="club-details-container">
+            <div className={`club-details ${activeClub !== null ? 'active' : ''}`}>
+              <div className="club-icon">
+                <img src={clubs[activeClub].logo} alt={`Logo ${clubs[activeClub].name}`} />
               </div>
-              <h3>{club.name}</h3>
-              <p>{club.description}</p>
-              <button>{club.buttonLabel}</button>
+              {joinFormVisible ? (
+                <div className="join-form">
+                  <h3>Join {clubs[activeClub].name}</h3>
+                  <form>
+                    <div className="form-group">
+                      <input type="text" placeholder="Name" />
+                    </div>
+                    <div className="form-group">
+                      <input type="email" placeholder="Email" />
+                    </div>
+                    <button>Submit</button>
+                  </form>
+                </div>
+              ) : (
+                <div className="club-info">
+                  <h2>{clubs[activeClub].name}</h2>
+                  <p>{clubs[activeClub].description}</p>
+                  <button onClick={() => handleJoinUs(activeClub)} className="join-button">Join Us</button>
+                </div>
+              )}
             </div>
           </div>
-        ))}
-      </Slider>
+        </div>
+      ) : (
+        <Slider {...settings}>
+          {clubs.map((club, index) => (
+            <div className="card-wrapper" key={index}>
+              <div className="card" style={{ background: club.background }}>
+                <div className="circle">
+                  <img src={club.logo} alt={`Logo ${club.name}`} />
+                </div>
+                <h3>{club.name}</h3>
+                {!showDetails && (
+                  <button onClick={() => handleLearnMore(index)}>En savoir plus</button>
+                )}
+              </div>
+            </div>
+          ))}
+        </Slider>
+      )}
       <style jsx>{`
-        .clubs {
+        /* Existing styles... */
+/* Existing styles... */
+
+	.clubs {
           text-align: center;
         }
 
+        h1 {
+          font-family: Montserrat, sans-serif;
+          letter-spacing: 2px;
+          font-size: 35px;
+          font-weight: 400;
+          margin: 15px 10px;
+        }
+
         .card-wrapper {
-          margin: 20px 10px; /* Add margin for spacing on both sides */
-          background-color: white
+          margin: 20px 10px;
+          background-color: white;
         }
 
         .card {
@@ -117,16 +186,21 @@ const Clubs = () => {
           object-fit: cover;
         }
 
+        h2 {
+          font-size: 24px;
+          margin-bottom: 10px;
+        }
+
         h3 {
-          margin: 10px 10px
+          margin: 10px 10px;
         }
 
         p {
-          margin: 10px 10px
+          margin: 10px 10px;
         }
 
         .card button {
-          background-color: #4CAF50;
+          background-color: #4caf50;
           color: white;
           padding: 10px 20px;
           border: none;
@@ -136,6 +210,179 @@ const Clubs = () => {
         }
 
         .card button:hover {
+          background-color: #45a049;
+        }
+
+        .club-details-container {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+          margin-top: 40px;
+          
+        }
+
+        .club-details {
+          display: flex;
+          align-items: center;
+          width: 500px;
+          background-color: #f5f5f5;
+          padding: 20px;
+          margin: 10px;
+          border-radius: 5px;
+          text-align: center;
+          transition: transform 0.3s ease-in-out;
+          border-radius: 5px;
+          text-align: left;
+          transition: transform 0.3s ease-in-out;
+          box-shadow: 0 2px 6px rgba(0, 0, 0, 0.2);
+        }
+
+        .club-details.active {
+          transform: scale(1.1);
+        }
+
+        .club-icon {
+          width: 100px;
+          height: 100px;
+          border-radius: 50%;
+          overflow: hidden;
+          margin-right: 20px;
+        }
+
+        .club-info {
+          text-align: left;
+        }
+
+        .back-icon {
+          cursor: pointer;
+          font-size: 24px;
+          color: #333;
+          margin-bottom: 10px;
+        }
+
+        .join-form {
+          margin-top: 20px;
+          text-align: center;
+        }
+
+        .join-form h3 {
+          margin-bottom: 10px;
+        }
+
+        .join-form form {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-top: 20px;
+        }
+
+        .join-form input {
+          margin-bottom: 10px;
+          padding: 5px;
+          width: 200px;
+        }
+
+        .join-form button {
+          background-color: #4caf50;
+          color: white;
+          padding: 10px 20px;
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+        }
+
+        .join-form button:hover {
+          background-color: #45a049;
+        }
+
+        .join-button {
+          background-color: #4caf50;
+          color: white;
+          padding: 10px 20px;
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+          font-size: 16px;
+        }
+      
+        .join-button:hover {
+          background-color: #45a049;
+        }
+
+        .join-button {
+          background-color: #4caf50;
+          color: white;
+          padding: 10px 20px;
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+          font-size: 16px;
+        }
+
+        .join-button:hover {
+          background-color: #45a049;
+        }
+        /* Add or modify styles as needed */
+        .join-form {
+          margin-top: 20px;
+          text-align: center;
+        }
+
+        .join-form h3 {
+          margin-bottom: 10px;
+        }
+
+        .join-form form {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin-top: 20px;
+        }
+
+        .join-form .form-group {
+          margin-bottom: 15px;
+        }
+
+        .join-form .form-group label {
+          display: block;
+          font-weight: bold;
+          margin-bottom: 5px;
+        }
+
+        .join-form .form-group input,
+        .join-form .form-group textarea {
+          padding: 10px;
+          border: 1px solid #ccc;
+          border-radius: 5px;
+          width: 300px;
+          max-width: 100%;
+        }
+
+        .join-form button {
+          background-color: #4caf50;
+          color: white;
+          padding: 10px 20px;
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+          font-size: 16px;
+        }
+
+        .join-form button:hover {
+          background-color: #45a049;
+        }
+
+        .join-button {
+          background-color: #4caf50;
+          color: white;
+          padding: 10px 20px;
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+          font-size: 16px;
+        }
+
+        .join-button:hover {
           background-color: #45a049;
         }
       `}</style>
