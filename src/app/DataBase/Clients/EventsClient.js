47658c4cf76eb6273c783970b4dc7b47302d7479
Shadowx9920 <youssefoutahar1@gmail.com>
2023-06-09 14:52:15 +0100

    started working on DB Clients

---
+++ src/app/DataBase/Clients/EventsClient.js
@@ -0,0 +1,68 @@
+import supabase from "./SupabaseClient";
+import { getClub } from "./ClubsClient";
+
+export const addEvent = async (event) => {
+    const { data, error } = await supabase.from("Activites").insert([event]);
+    if (error) {
+        console.error("Error adding event:", error);
+    } else {
+        console.log("Event added successfully");
+    }
+};
+
+export const getEvents = async () => {
+    const { data, error } = await supabase.from("Activites").select("*");
+    if (error) {
+        console.error("Error fetching events:", error);
+    } else {
+        console.log("Fetched events:", data);
+        return data;
+    }
+};
+
+export const getEvent = async (id) => {
+    const { data, error } = await supabase.from("Activites").select("*").eq("id", id);
+    if (error) {
+        console.error("Error fetching event:", error);
+    } else {
+        console.log("Fetched event:", data);
+        return data;
+    }
+};
+
+export const updateEvent = async (id, event) => {
+    const { data, error } = await supabase.from("Activites").update(event).eq("id", id);
+    if (error) {
+        console.error("Error updating event:", error);
+    } else {
+        console.log("Event updated successfully");
+    }
+};
+
+export const deleteEvent = async (id) => {
+    const { data, error } = await supabase.from("Activites").delete().eq("id", id);
+    if (error) {
+        console.error("Error deleting event:", error);
+    } else {
+        console.log("Event deleted successfully");
+    }
+};
+
+export const getEventClub = async (id) => {
+    let { data , error } = await supabase.from("club_activity").select("club_id").eq("id", id).limit(1);
+    if (error) {
+        console.error("Error fetching event club:", error);
+    } else {
+        return getClub(data[0].club_id);
+    }
+};
+
+export const addClubToEvent = async (club_id, event_id) => {
+    const { data, error } = await supabase.from("club_activity").insert([{ club_id: club_id, activity_id: event_id }]);
+    if (error) {
+        console.error("Error adding club to event:", error);
+    } else {
+        console.log("Club added to event successfully");
+    }
+}
+
--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,32 @@ import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 
 
+//Testing Clients :
+// import { getClub , addClub,deleteClub,getClubEvents,getClubs,updateClub } from './app/DataBase/Clients/ClubsClient'
+// import {addEvent,deleteEvent,getEvent,getEventClub,getEvents,updateEvent} from './app/DataBase/Clients/EventsClient'
+
+// addClub(
+//   {
+//     nom: "Club 1",
+//     nb_membres: 10,
+//     id_category: 1,
+//     description: "Club 1 description",
+//     logo: "https://www.uir.ac.ma/upload/cbuilder/c076f4b3518ce8be7f84bb05ec8ce0b2cc0f366f.png",
+//   }
+// );
+
+// addEvent(
+//   {
+//     image:"src\Images\event1.png",
+//     Name:"Event 1",
+//     Date : "2021-10-10",
+//     Lieu : "UIR",
+//     Cost : 100,
+//     Earnings : 200,
+//     Supp_budget : 100,
+//     id_club 
+
+
 ReactDOM.render(
   <StyledEngineProvider injectFirst>
     <BrowserRouter>
