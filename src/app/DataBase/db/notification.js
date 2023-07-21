846d3e7780c8feb2f6b9f446d34d61a0d0d4491d
Shadowx9920 <youssefoutahar1@gmail.com>
2023-07-21 17:14:56 +0100

    last update

--- src/app/DataBase/db/notification.js
+++ src/app/DataBase/db/notification.js
@@ -5,26 +5,26 @@ import { getCurrentUser , getProfileById } from '../Clients/UsersClient';
 Mock.onGet('/api/notification').reply(async(config) => {
     let user = await getCurrentUser()
     let profile = await getProfileById(user.id)
+    const response = await getNotifications()
     
-    let notifications = [];
-    if (profile.role == "admin") {
-        const response = await getNotifications()
-        console.log(response.data)
-        response.data.forEach(async (notification) => {
-            if (notification.id_club != null) {
-                notifications.push(notification)
-            }
-        });
-        return [200, notifications]
-    } else {
-        const response = await getNotifications()
-        response.data.forEach(async (notification) => {
-            if (notification.id_club == null) {
-                notifications.push(notification)
-            }
-        });
-        return [200, notifications]
-    }
+    // let notifications = [];
+    // if (profile.role == "admin") {
+    //     response.data.forEach(async (notification) => {
+    //         if (notification.id_club != null) {
+    //             notifications.push(notification)
+    //         }
+    //     });
+    //     return [200, notifications]
+    // } else {
+    //     const response = await getNotifications()
+    //     response.data.forEach(async (notification) => {
+    //         if (notification.id_club == null) {
+    //             notifications.push(notification)
+    //         }
+    //     });
+    //     return [200, notifications]
+    // }
+    return [200, response.data]
 })
 
 Mock.onPost('/api/notification/add').reply((config) => {
