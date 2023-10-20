171d8ed246234e20e89aec9a8cd33694bf7724fb
ed-daoudiManal22 <manaloglu@gmail.com>
2023-10-20 18:00:35 +0100

    frm

--- src/app/views/Forum/ForumPage.js
+++ src/app/views/Forum/ForumPage.js
@@ -1,119 +1,194 @@
-import React, { useState, useEffect } from "react";
-import { Box,TextField, IconButton, List, ListItem, ListItemText,styled } from "@mui/material";
+import React, { useState, useEffect, useRef } from "react";
+import {
+  Box,
+  TextField,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  styled,
+  Typography,
+  Button,
+  Paper,
+} from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
 import { MatxLoading } from "../../components";
 import { getForumFromClub } from "../../DataBase/services/ForumsService";
 import { getMembreClub } from "../../DataBase/services/MembersService";
 import { getCurrentUser, getUserMember } from "../../DataBase/services/UsersService";
 import { getMessagesByForum, addMessage } from "../../DataBase/services/MessagesService";
-import SimpleCard from '../../components/SimpleCard';
+import SimpleCard from "../../components/SimpleCard";
+
+// Styled components for custom styling
+const ForumContainer = styled(Paper)(({ theme }) => ({
+  background: "#fff",
+  borderRadius: "8px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: theme.spacing(2),
+  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+  maxHeight: "80vh", // Adjust the maximum height as needed
+  overflowY: "auto",
+}));
+
+const MessageList = styled(List)(({ theme }) => ({
+  flex: "1",
+  overflowY: "auto",
+  width: "100%",
+  padding: theme.spacing(2),
+}));
+
+const MessageItem = styled(ListItem)(({ theme, isCurrentUser }) => ({
+  backgroundColor: isCurrentUser ? "#4caf50" : "#2196f3",
+  borderRadius: theme.spacing(2),
+  marginBottom: theme.spacing(2),
+  wordWrap: "break-word",
+  padding: theme.spacing(2),
+  color: "#fff",
+}));
+
+const Header = styled(Typography)(({ theme }) => ({
+  fontSize: "24px",
+  fontWeight: "bold",
+  marginBottom: theme.spacing(2),
+}));
+
+const MessageInputContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  padding: theme.spacing(2),
+  borderTop: "1px solid #ccc",
+}));
 
 const Forums = () => {
-    const [user, setUser] = useState();
-    const [forum, setforums] = useState();
+  const [user, setUser] = useState();
+  const [forum, setForum] = useState();
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [messagesToShow, setMessagesToShow] = useState(10); // Number of messages to display initially
+  const messageListRef = useRef();
+  const loadingMoreMessagesRef = useRef(false);
 
-    useEffect(() => {
-        getCurrentUser().then((CurrentUser) => {
-            getUserMember(CurrentUser.id).then((member) => {
-                getMembreClub(member[0].id).then((club) => {
-                    getForumFromClub(club[0].id).then((forum) => {
-                        setUser(CurrentUser);
-                        setforums(forum[0]);
-                    });
-                });
-            });
+  useEffect(() => {
+    getCurrentUser().then((CurrentUser) => {
+      getUserMember(CurrentUser.id).then((member) => {
+        getMembreClub(member[0].id).then((club) => {
+          getForumFromClub(club[0].id).then((forum) => {
+            setUser(CurrentUser);
+            setForum(forum[0]);
+          });
         });
+      });
     }, []);
+  }, []);
 
-    const [message, setMessage] = React.useState("");
-    const [messages, setMessages] = React.useState([]);
+  useEffect(() => {
+    if (forum) {
+      getMessagesByForum(forum.id).then((newMessages) => {
+        newMessages.sort((a, b) => b.date_creation - a.date_creation);
+        setMessages(newMessages);
+      });
+    }
+  }, [forum]);
 
-    useEffect(() => {
-        if (forum) {
-            getMessagesByForum(forum.id).then((messages) => {
-                setMessages(messages);
-            });
-        }
-    }, [forum]);
+  useEffect(() => {
+    if (messageListRef.current) {
+      messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+    }
+  }, [messages]);
 
-    const handleInputChange = (event) => {
-        setMessage(event.target.value);
-    };
+  const handleInputChange = (event) => {
+    setMessage(event.target.value);
+  };
 
-    const handleSendMessage = async () => {
-        if (message.trim() !== "") {
-            const newMessage = {
-                forum_id: forum.id,
-                user_id: user.id,
-                content: message,
-            };
+  const handleSendMessage = async () => {
+    if (message.trim() !== "") {
+      const newMessage = {
+        forum_id: forum.id,
+        user_id: user.id,
+        content: message,
+      };
 
-            await addMessage(newMessage);
+      await addMessage(newMessage);
 
-            setMessages([...messages, newMessage]);
-            setMessage("");
-        }
-    };
-    return (
-        <SimpleCard>
-            <Box sx={{ position: "relative", marginLeft: "1%", marginRight: "1%", marginTop: "1%" }}>
-            <div className="header">
-                <h1>Forums</h1>
-            </div>
+      setMessages([newMessage, ...messages]);
+      setMessage("");
+    }
+  };
 
-            {forum ? (
-                <Box sx={{ flex: "1 1 auto", borderRadius: "4px", overflow: "auto", p: 1 }}>
-                    <Box
-                        sx={{
-                            borderRadius: "4px",
-                            overflow: "auto",
-                            maxHeight: "350px",
-                            p: 1,
-                        }}
-                    >
-                        <List>
-                            {messages.map((msg, index) => (
-                                <ListItem
-                                    key={msg.id}
-                                    sx={{
-                                        textAlign: msg.user_id === user.id ? "right" : "left",
-                backgroundColor: msg.user_id === user.id ? "lightBlue" : "grey",
-                borderRadius: "4px",
-                marginBottom: "8px",
-                marginLeft: msg.user_id === user.id ? "auto" : "initial",
-                maxWidth: "65%",
-                wordWrap: "break-word",
-                                    }}
-                                >
-                                    <ListItemText primary={msg.content} secondary={msg.date_creation}/>
-                                </ListItem>
-                            ))}
-                        </List>
-                    </Box>
-                    <Box
-                        sx={{ position: "relative", flexShrink: 0, display: "flex", alignItems: "center" }}
-                    >
-                        <TextField
-                            label="Message"
-                            value={message}
-                            onChange={handleInputChange}
-                            variant="outlined"
-                            fullWidth
-                            sx={{ mr: 1 }}
-                        />
-                        <IconButton color="primary" onClick={handleSendMessage}>
-                            <SendIcon />
-                        </IconButton>
-                    </Box>
-                </Box>
-            ) : (
-                <div className="centered-and-flexed">
-                    <MatxLoading />
-                </div>
+  const handleLoadMoreMessages = async () => {
+    if (!loadingMoreMessagesRef.current) {
+      loadingMoreMessagesRef.current = true;
+      const oldestMessageDate = messages[messagesToShow - 1]?.date_creation;
+      const olderMessages = await getMessagesByForum(forum.id, oldestMessageDate);
+
+      if (olderMessages.length > 0) {
+        setMessages([...olderMessages, ...messages]); // Prepend older messages
+        setMessagesToShow(messagesToShow + olderMessages.length); // Increase the number of messages to show
+      }
+
+      loadingMoreMessagesRef.current = false;
+    }
+  };
+
+  return (
+    <SimpleCard>
+      <ForumContainer>
+        <Header variant="h1">Forums</Header>
+        {forum ? (
+          <Box sx={{ flex: "1", width: "100%", display: "flex", flexDirection: "column" }}>
+            <MessageList ref={messageListRef} onScroll={handleLoadMoreMessages}>
+              {messages.slice(0, messagesToShow).map((msg, index) => (
+                <MessageItem key={msg.id} isCurrentUser={msg.user_id === user.id}>
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <img
+                      src={msg.profilePictureUrl} // Assuming you have a URL for the profile picture
+                      alt={msg.username}
+                      style={{ width: "40px", height: "40px", borderRadius: "50%", marginRight: "8px" }}
+                    />
+                    <div>
+                      <ListItemText
+                        primary={`${msg.username} - ${new Date(msg.date_creation).toLocaleString()}`}
+                        secondary={msg.content}
+                        secondaryTypographyProps={{ style: { color: "white" } }}
+                      />
+                    </div>
+                  </div>
+                </MessageItem>
+              ))}
+            </MessageList>
+            {messages.length > messagesToShow && (
+              <div style={{ display: "flex", justifyContent: "center", marginTop: "16px" }}>
+                <Button variant="outlined" onClick={handleLoadMoreMessages}>
+                  Load More Messages
+                </Button>
+              </div>
             )}
-        </Box>
-        </SimpleCard>
-    );
+            <MessageInputContainer>
+              <TextField
+                label="Message"
+                value={message}
+                onChange={handleInputChange}
+                variant="outlined"
+                fullWidth
+                sx={{ marginRight: 1 }}
+                inputProps={{ style: { color: "gray" } }}
+              />
+              <IconButton color="primary" onClick={handleSendMessage}>
+                <SendIcon />
+              </IconButton>
+            </MessageInputContainer>
+          </Box>
+        ) : (
+          <div className="centered-and-flexed">
+            <MatxLoading />
+          </div>
+        )}
+      </ForumContainer>
+    </SimpleCard>
+  );
 };
 
 export default Forums;
