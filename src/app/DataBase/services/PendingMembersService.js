bcd0691544437be3f880244e10d43963c2dc9425
ManalChailou <chailou.manal@gmail.com>
2023-10-20 17:53:57 +0100

    updated

---
+++ src/app/DataBase/services/PendingMembersService.js
@@ -0,0 +1,26 @@
+import supabase from "../Clients/SupabaseClient";
+import ClubsService from "./ClubsService";
+
+export default class PendingMembersService {
+    static async addMember(user) {
+        try {
+            const { data, error } = await supabase.from("Pending_members").insert([user]);
+            if (error) throw error;
+            return data;
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    static async getMemberById(id) {
+        try {
+            const { data, error } = await supabase.from("Pending_members").select("*").eq("id", id);
+            if (error) throw error;
+            return data;
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    
+}
