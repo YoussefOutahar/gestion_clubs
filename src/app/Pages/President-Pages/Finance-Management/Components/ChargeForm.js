8235073297f2496455278b81d15da3f5ba19ad16
ManalChailou <chailou.manal@gmail.com>
2023-10-22 20:14:42 +0100

    organizing folders

---
+++ src/app/Pages/President-Pages/Finance-Management/Components/ChargeForm.js
@@ -0,0 +1,216 @@
+import { Button, Grid, Icon, styled, MenuItem, Box } from "@mui/material";
+import { Span } from "../../../components/Typography";
+import { useEffect, useState } from "react";
+import { TextValidator, ValidatorForm } from "react-material-ui-form-validator";
+import supabase from "../../../DataBase/Clients/SupabaseClient";
+import { useNavigate } from "react-router-dom";
+
+const TextField = styled(TextValidator)(() => ({
+    width: "100%",
+    marginBottom: "16px",
+}));
+
+const ChargeForm = () => {
+    const navigate = useNavigate();
+
+    const [state, setState] = useState({
+        date: new Date(),
+        eventName: "",
+        activities: [],
+    });
+
+    useEffect(() => {
+        const fetchActivities = async () => {
+            try {
+                const { data, error } = await supabase.from("Events").select("Name");
+                if (error) {
+                    console.error("Error fetching activities:", error);
+                } else {
+                    setState((prevState) => ({
+                        ...prevState,
+                        activities: data.map((activity) => activity.Name),
+                    }));
+                }
+            } catch (error) {
+                console.error("Error fetching activities:", error);
+            }
+        };
+
+        fetchActivities();
+    }, []);
+
+    {
+        /*useEffect(() => {
+    ValidatorForm.addValidationRule("isInvoiceMatch", (value) => {
+      if (value !== state.Invoice) return false;
+
+      return true;
+    });
+
+    return () => {
+      ValidatorForm.removeValidationRule("isInvoiceMatch");
+    };
+  }, [state.Invoice]);*/
+    }
+
+    //TODO:
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (selectedFile) {
+            try {
+                setUploading(true);
+                const { file, error } = await supabase.storage
+                    .from("Budget_event")
+                    .upload(selectedFile.name, selectedFile);
+
+                if (error) {
+                    console.error("Error uploading file:", error.message);
+                } else {
+                    const fileUrl = "https://vussefkqdtgdosoytjch.supabase.co/storage/v1/object/public/Budget_event/" + selectedFile.name;
+                    const { data, error } = await supabase
+                        .from("Events")
+                        .update([
+                            {
+                                Name: state.eventName,
+                                Cost: state.totalCost,
+                                Earnings: state.Earning,
+                                //Supp_budget: state.suppBudget,
+                                File_name: selectedFile.name,
+                                Url: fileUrl,
+                            },
+                        ])
+                        .eq("Name", state.eventName);
+
+                    if (error) {
+                        console.error(error);
+                    } else {
+                        console.log("Data inserted successfully:", data);
+                    }
+                    console.log("File uploaded and reference saved successfully.");
+                }
+            } catch (error) {
+                console.error("Error uploading file:", error.message);
+            } finally {
+                setUploading(false);
+            }
+        }
+        // try {
+        //
+
+        //     const file = event.target.Invoice.files[0];
+        //     const { data: fileData, error: fileError } = await supabase.storage
+        //         .from("Budget_event")
+        //         .upload(`invoices/${file.name}`, file);
+
+        //     if (fileError) {
+        //         console.error(fileError);
+        //     } else {
+        //         console.log("File uploaded successfully:", fileData);
+        //     }
+        //     // Redirect to finance page
+        //     navigate("/finance");
+        // } catch (error) {
+        //     console.error(error);
+        // }
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setState((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const { eventName, totalCost, Earning, suppBudget, activities } = state;
+
+    //Invoice
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [uploading, setUploading] = useState(false);
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    return (
+        <div>
+            <ValidatorForm onSubmit={handleSubmit} onError={() => null}>
+                <Grid container spacing={6}>
+                    <Grid item lg={6} md={6} sm={12} xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            select
+                            name="eventName"
+                            value={eventName || ""}
+                            onChange={handleChange}
+                            errorMessages={["this field is required"]}
+                            label="Event Name"
+                            validators={["required"]}
+                        >
+                            {activities.map((activity, index) => (
+                                <MenuItem key={index} value={activity}>
+                                    {activity}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+
+                        <TextField
+                            type="number"
+                            name="totalCost"
+                            label="Total Cost"
+                            onChange={handleChange}
+                            value={totalCost || ""}
+                            validators={["required"]}
+                            errorMessages={["this field is required"]}
+                        />
+
+                        <TextField
+                            type="number"
+                            name="Earning"
+                            label="Earnings"
+                            onChange={handleChange}
+                            value={Earning || ""}
+                            errorMessages={["this field is required"]}
+                            validators={["required"]}
+                        />
+
+                        {/*<TextField
+                            type="text"
+                            name="suppBudget"
+                            value={suppBudget || ""}
+                            label="supplementary Budget"
+                            onChange={handleChange}
+                            validators={["required"]}
+                            errorMessages={["this field is required"]}
+                            />*/}
+                        <Grid container direction="row" justifyContent="center" alignItems="center" gap={2}>
+                            <input
+                                type="file"
+                                onChange={handleFileChange}
+                                accept=".pdf,.doc,.docx"
+                                id="invoice-input"
+                                style={{ display: "none" }}
+                            />
+                            <label htmlFor="invoice-input">
+                                <Button sx={{ mb: 4 }} color="primary" component="span" variant="outlined">
+                                    Upload Invoice
+                                </Button>
+                            </label>
+
+                            <Span style={{ display: "inline-flex", verticalAlign: "middle" }}>
+                                {uploading ? "Uploading ..." : null}
+                                {selectedFile ? selectedFile.name : null}
+                            </Span>
+                        </Grid>
+                    </Grid>
+                </Grid>
+
+                <Button color="primary" variant="contained" type="submit" marginTop="20px">
+                    <Icon>send</Icon>
+                    <Span sx={{ pl: 1, textTransform: "capitalize" }}>Submit</Span>
+                </Button>
+            </ValidatorForm>
+        </div>
+    );
+};
+
+export default ChargeForm;
---
+++ src/app/Pages/President-Pages/Finance-Management/Components/ChargesTable.js
@@ -0,0 +1,77 @@
+import {Box,styled,Table,TableBody,TableCell,TableHead,TablePagination,TableRow,} from "@mui/material";
+import { useState } from "react";
+  
+  const StyledTable = styled(Table)(() => ({
+    whiteSpace: "pre",
+    "& thead": {
+      "& tr": { "& th": { paddingLeft: 0, paddingRight: 0 } },
+    },
+    "& tbody": {
+      "& tr": { "& td": { paddingLeft: 0, textTransform: "capitalize" } },
+    },
+  }));
+  
+  
+  const ChargesTable= ({ MyData}) => {
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+  
+    const handleChangePage = (_, newPage) => {
+      setPage(newPage);
+    };
+  
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(+event.target.value);
+      setPage(0);
+    };
+
+    const formatDate = (timestamp) => {
+      const date = new Date(timestamp);
+      return date.toLocaleDateString();
+    };
+  
+    return (
+      <Box width="100%" overflow="auto">
+        <StyledTable>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Event Name</TableCell>
+            <TableCell align="center">Date</TableCell>
+            <TableCell align="center">Cost</TableCell>
+            <TableCell align="center">Earned</TableCell>
+            <TableCell align="center">Supplimentary budget</TableCell>
+          </TableRow>
+        </TableHead>
+          <TableBody>
+            {MyData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((data, index) => (
+                <TableRow key={index}>
+                <TableCell align="center">{data.Name}</TableCell>
+                <TableCell align="center">{formatDate(data.Date)}</TableCell>
+                <TableCell align="center">{data.Cost}</TableCell>
+                <TableCell align="center">{data.Earnings}</TableCell>
+                <TableCell align="center">{data.Supp_budget}</TableCell>
+              </TableRow>
+              ))}
+          </TableBody>
+        </StyledTable>
+  
+        <TablePagination
+          sx={{ px: 2 }}
+          page={page}
+          component="div"
+          rowsPerPage={rowsPerPage}
+          count={MyData.length}
+          onPageChange={handleChangePage}
+          rowsPerPageOptions={[5, 10, 25]}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          nextIconButtonProps={{ "aria-label": "Next Page" }}
+          backIconButtonProps={{ "aria-label": "Previous Page" }}
+        />
+      </Box>
+    );
+  };
+  
+  export default ChargesTable;
+  
